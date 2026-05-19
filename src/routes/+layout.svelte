e1eafa46c694fdb204b139efaa345f23ffdc1d09
dubina <lightplay577@gmail.com>
2026-05-19 17:09:02 +0300

    ...

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,8 @@
 <script>
+
+	import '../../static/styles/nullstyle.css';
+  	import '../../static/styles/roots.css';
+  	import '../../static/styles/design_system.css';
 	let { children } = $props();
 </script>
 
